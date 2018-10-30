bdc40193c36193869aef2168168025ebaa081daf
Quan Dao <daq30899@gmail.com>
2018-10-30 19:19:34 +0200

    Completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class chooseBackgroundColor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundColor: "red",
+    };
+    this.changeBackgroundColor = this.changeBackgroundColor.bind(this);
+  }
+
+  changeBackgroundColor(event) {
+    this.setState({backgroundColor: event.target.value});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="App-header" style={this.state}>
+          <div className="select-box">
+            <select value={this.state.value} onChange={this.changeBackgroundColor}>
+              <option value="red">Red</option>
+              <option value="white">White</option>
+              <option value="black">Black</option>
+              <option value="aquamarine">Aquamarine</option>
+              <option value="blue">Blue</option>
+            </select>
+          </div>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default chooseBackgroundColor;
